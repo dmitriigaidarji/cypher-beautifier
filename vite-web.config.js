ed3d884a2c9ec2e68e00b7fd338c71f2c7a9004a
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2024-09-23 02:15:33 +0800

    fix base path

--- vite-web.config.js
+++ vite-web.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "cypher-beautifier",
   build: {
     // rollupOptions: {
     //   input: "./src/web/main.ts",
