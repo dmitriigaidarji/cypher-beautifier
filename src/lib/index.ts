2f6dde14dcf1f2685074e295913e263d9a60ab69
Dmitrii Gaidarji <dmitriigaidarji@gmail.com>
2024-09-23 22:08:54 +0800

    update web ui

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,24 @@
+const allKeywords = [
+  "CASE",
+  "AND",
+  "OR",
+  "WHEN",
+  "XOR",
+  "DISTINCT",
+  "IN",
+  "AS",
+  "STARTS WITH",
+  "ENDS WITH",
+  "CONTAINS",
+  "NOT",
+  "OPTIONAL MATCH",
+  "MERGE",
+  "MATCH",
+  "RETURN",
+  "SET",
+  "ORDER BY",
+  "LIMIT",
+];
 function beautifyCypher(query: string) {
   // spaces
   query = query.replace(
@@ -9,28 +30,65 @@ function beautifyCypher(query: string) {
 
   // new lines
   query = query.replace(
-    /\b(OPTIONAL MATCH|MATCH|RETURN|SET|ORDER BY|LIMIT)\b/gi,
+    /\b(OPTIONAL MATCH|MERGE|MATCH|RETURN|SET|ORDER BY|LIMIT)\b/gi,
     function (match) {
-      return "\n" + match.toUpperCase() + " ";
+      const v = match.toUpperCase();
+      switch (v) {
+        case "SET":
+          return "\n" + v + "\n";
+        default:
+          return "\n" + v + " ";
+      }
     },
   );
 
   query = query.replace(/  +/g, " ");
-
-  if (query[0] === "\n") {
-    query = query.substring(1);
-  }
 
   // no spaces before
   query = query.replace(/( :)/gi, function (match) {
     return match.trim();
   });
 
+  // no double new lines
   query = query
     .split("\n")
     .map((t) => t.trim())
     .filter((t) => t.length > 0)
     .join("\n");
+
+  // add double new lines
+  query = query.replace(/\b(MERGE)\b/gi, function (match) {
+    return "\n" + match.toUpperCase() + " ";
+  });
+
+  // add indents
+  let currentIndent = 0;
+  const indentSize = 6;
+  query = query
+    .split("\n")
+    .map((t) => {
+      if (t.startsWith("SET")) {
+        currentIndent += 2;
+        return (
+          Array((currentIndent - 1) * indentSize)
+            .fill(" ")
+            .join("") + t
+        );
+      } else if (allKeywords.includes(t.split(" ")[0])) {
+        currentIndent = 0; //Math.max(0, currentIndent - 1);
+      }
+      return (
+        Array(currentIndent * indentSize)
+          .fill(" ")
+          .join("") + t
+      );
+    })
+    .join("\n");
+
+  if (query[0] === "\n") {
+    query = query.substring(1);
+  }
+
   console.log(query);
 
   return query;
--- src/web/main.ts
+++ src/web/main.ts
@@ -1,11 +1,39 @@
 import "./style.css";
 import beautifyCypher from "../lib/index.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <div>${beautifyCypher("MATCH   p=()-() RETURN p limit 10;")
-      .split("\n")
-      .map((t) => `<p>${t}</p>`)}</div>
-    <h1>Cypher Beautifier</h1>
-  </div>
-`;
+const input = document.getElementById("input") as HTMLTextAreaElement;
+
+const output = document.getElementById("output") as HTMLTextAreaElement;
+
+function beautify() {
+  output.value = beautifyCypher(input.value);
+}
+
+input.addEventListener("input", () => {
+  beautify();
+});
+//
+// input.value = `
+// MERGE (sm:SiteMaterial {node_key: 'SM_'+ coalesce(row.material,'Unknown') + '_' + coalesce(row.site,'Unknown')})
+// ON CREATE
+// \tSET sm.material_id = row.material,sm.site_id = row.site,
+// \t\tsm.updated_on = datetime()
+//
+// MERGE (sdi)-[:SPECIFIES]->(sm)
+//
+// MERGE (material:Material {node_key: 'MAT_'+coalesce(row.material,'Unknown') })
+//     ON CREATE
+//     \tSET material.material_id = coalesce(row.material,'Unknown'),
+//     \t\tmaterial.updated_on = datetime()
+//
+// MERGE (sm)-[:COMPOSED_OF]->(material)
+//
+// MERGE (inventory:Warehouse:Inventory {node_key: 'WHS_'+ coalesce(row.batch,'Unknown') + '_' + coalesce(row.material,'Unknown') + '_' + coalesce(row.site,'Unknown')})
+// ON CREATE
+// \t\tSET inventory.batch_id =  row.batch,
+// \t\t\tinventory.material_id = row.material,
+// \t\t\tinventory.site_id = row.site,
+// \t\t\tinventory.updated_on = datetime()
+// `;
+//
+// beautify();
